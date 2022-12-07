d43b549ef9cbdb1b7ee02608343babb8c1d22871
= <=>
2022-12-06 21:09:01 -0600

    to do

--- TeamWork/src/components/Todo.jsx
+++ TeamWork/src/components/Todo.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Button,  ScrollView, Image,
+import { View, Modal,  Text, StyleSheet, TouchableOpacity, Dimensions, Button,  ScrollView, Image,
      TextInput} from 'react-native';
 
 import Tarjeta from './Tarjeta';
 import close from '../images/X.png'
-import ModalInput from './ModalInput';
 
 const alto = Dimensions.get ('window').height
 const ancho = Dimensions.get ('window').width
@@ -25,7 +24,67 @@ const Todo = () => {
         
         </ScrollView>
 
-         <ModalInput view={view}/>
+            <Modal animationType='fade' 
+            onDismiss={ () => console.log("close") }
+            onShow={ () => console.log("show")} 
+            visible = {view}>
+
+                <View style= {{
+                    flex: 1, 
+                    backgroundColor: "rgba(1, 1, 1, 0.5)",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+
+                    <View style= {{
+                        height: 650,
+                        width: 330,
+                        backgroundColor: "#B9C3CD",
+                        borderBottomEndRadius: 20,
+                        borderBottomStartRadius: 20, 
+                        borderTopRightRadius: 20,
+                        borderTopLeftRadius: 20,
+                    }}>
+
+                        <View
+                        style= {{
+                            height: 45,
+                            width: -130,
+                            flexDirection: "row",
+                            justifyContent: "flex-end",
+                            alignItems: "center",
+                            paddingHorizontal: 10, 
+                            paddingVertical: 10,
+                        }}>
+                            
+                            <TouchableOpacity
+                            onPress={() => setView(false)}
+                            >
+
+                            <Image
+                                source ={close}
+                                style = {{
+                                    height: 20,
+                                    width: 40,
+                                }}
+                            />
+                            </TouchableOpacity>
+
+                        </View> 
+                        <Text> ASIGNACION: </Text>
+                        <TextInput> </TextInput>
+                        <Text> FECHA LIMITE: </Text>
+                        <TextInput></TextInput>
+                        <Text> CONCEPTO: </Text>
+                        
+                        
+
+
+                    </View>
+                    
+                </View>
+
+            </Modal>
 
         </View>
     )
